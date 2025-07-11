19b93e72d0a801bacc95bfe9a5314abe3c814d3d
LanceSissing <lance.sissing@me.com>
2025-07-11 17:08:42 +0100

    Prep for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app-uniqu-name.wsgi
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a0*!rdi@swr)45ej5s0s00#k6e^%9)gsb@po__^-pbp#iq)3*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
